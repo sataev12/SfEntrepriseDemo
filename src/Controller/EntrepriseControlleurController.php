5560b1907ef406e7838d39de9658f727a9a09b12
sataev12 <sataevmagomed.eu@gmail.com>
2024-05-06 16:57:32 +0200

    Creation des vue pour afficher le texte ecrites sur dans symfony sur la page de navigateur

--- src/Controller/EntrepriseControlleurController.php
+++ src/Controller/EntrepriseControlleurController.php
@@ -11,8 +11,9 @@ class EntrepriseControlleurController extends AbstractController
     #[Route('/entreprise/controlleur', name: 'app_entreprise_controlleur')]
     public function index(): Response
     {
+        $name = 'Elan Formation';
         return $this->render('entreprise_controlleur/index.html.twig', [
-            'controller_name' => 'EntrepriseControlleurController',
+            'name' => $name
         ]);
     }
 }
